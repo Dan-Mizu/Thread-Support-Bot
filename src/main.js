79aa4315ebe0b04b9bf5237b9bc54600399d3a82
Dan <baghdadi.danny@gmail.com>
2022-07-03 03:19:00 -0700

    send image attachments

--- src/main.js
+++ src/main.js
@@ -1,29 +1,46 @@
-//dependencies
-const fs = require("fs");
-const path = require("path");
-
 //get servers
-const servers = JSON.parse(
-  fs.readFileSync(path.join(__dirname, "./config/servers.json")).toString()
-);
+const server_data = require("./config/server_data.json");
 
 //init discord.js
 const { Client, Intents } = require("discord.js");
 
 //loop through servers
-for (let i = 0; i < servers.length; i++) {
+for (let i = 0; i < server_data.length; i++) {
   //init instance
   const client = new Client({
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
   });
 
+  //create list of accepted channels
+  let channel_list = [];
+  for (let i2 = 0; i2 < server_data[i].channels.length; i2++) {
+    channel_list.push(server_data[i].channels[i2].channel_id);
+  };
+
   //events
-  client.on("messageCreate", (message) => {
-    if (message.content.includes("ping")) {
-      return message.reply("pong");
+  let sent_messages = [];
+  client.on('ready', client => {
+    for (let i2 = 0; i2 < channel_list.length; i2++) {
+      //remove older images
+      if (sent_messages) {
+        for (let i3 = 0; i3 < sent_messages.length; i3++) {
+          channel.messages.fetch(sent_messages[i3]).then(message => message.delete());
+        }
+      };
+
+      //send new images
+      client.channels.fetch(channel_list[i2]).then(channel => {
+        channel.send({ files: ['./resources/create_thread.png'] }).then(message => {
+          //store message id
+          sent_messages.push(message.id);
+        });
+      });
+
+      //DEBUG
+      console.log(sent_messages)
     }
   });
 
   //provide Client Token
-  client.login(servers[i].client_token);
+  client.login(server_data[i].client_token);
 }
